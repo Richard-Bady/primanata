0f2658dadb9298a1c46c929728a03706d5e69cfe
Ritch <richard.outtrabady@gmail.com>
2020-06-22 23:09:54 +0200

    Ajout lien dynamique pour les meta OG

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
 var domain = window.location.host;
 
 console.log('Le domaine du site est le suivant ---> ', domain, '<-------');
+
+var ogpImage = document.querySelector('.ogpImage');
+console.log('OGP image----> ', ogpImage.content);
+
+ogpImage.content = domain + ogpImage.content;
+
+console.log('OGP image after modification -> ', ogpImage.content);
