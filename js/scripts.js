8037d64dae9e8ccb263b98d0cc45ddf3e056e59c
Ritch <richard.outtrabady@gmail.com>
2020-06-22 23:46:06 +0200

    Ajout url pour les balises OG

--- js/scripts.js
+++ js/scripts.js
@@ -6,8 +6,10 @@ var domain = window.location.protocol + '//' + window.location.host + '/' + path
 console.log('Le domaine du site est le suivant ---> ', domain, '<-------');
 
 var ogpImage = document.querySelector('.ogpImage');
+var ogpUrl = document.querySelector('.ogpUrl');
 console.log('OGP image----> ', ogpImage.content);
 
 ogpImage.content = domain + ogpImage.content;
+ogpUrl.content = window.location.href;
 
 console.log('OGP image after modification -> ', ogpImage.content);
