1946434dfd67926b912cbfc3fbdcc5a9fd45e3e9
Ritch <richard.outtrabady@gmail.com>
2020-07-08 22:06:45 +0200

    Adding cookie banner + new pdf menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,40 @@
-var pathArray = window.location.pathname.split('/');
-var domain = window.location.protocol + '//' + window.location.host + '/' + pathArray[1];
+console.log('Script is correctly loaded');
+var scriptGoogleAnalytics = document.querySelector('#google-analytics-script');
 
-// console.log(window.location.pathname, window.location.hostname);
+// Handle cookies
+console.log('---------- Voici les cookies -------');
+console.log(document.cookie);
+console.log(document.cookie[0]);
+console.log(document.cookie.length);
+console.log('---------- Voici les cookies -------');
 
-console.log('Le domaine du site est le suivant ---> ', domain, '<-------');
+var userAcceptCookie = document.querySelector('.js-accept-analytics');
+var userDontAcceptCookie = document.querySelector('.js-dont-accept-analytics');
+var cookieBanner = document.querySelector('.cookie-banner');
 
-var ogpImage = document.querySelector('.ogpImage');
-var ogpUrl = document.querySelector('.ogpUrl');
-console.log('OGP image----> ', ogpImage.content);
 
-ogpImage.content = domain + ogpImage.content;
-ogpUrl.content = window.location.href;
+console.log('---------- Cookies Length-------');
+if(document.cookie.length === 0) {
+    console.log('EMPTY COOKIES')
+} else {
+    console.log('NOT EMPTY');
+}
+console.log('---------- Cookies Length-------');
 
-console.log('OGP image after modification -> ', ogpImage.content);
+userAcceptCookie.addEventListener('click', function() {
+    console.log('Cookie OK');
+    var cookieDate = new Date();
+    console.log(cookieDate);
+    console.log(cookieDate + 365);
+    
+    cookieBanner.classList.add('hide-cookie-banner')
+    document.cookie = 'userHasAcceptAnalytics=yes'; // Créé ou met à jour un cookie 'user'
+})
+
+userDontAcceptCookie.addEventListener('click', function() {
+    console.log('Pas cookie');
+    cookieBanner.classList.add('hide-cookie-banner')
+    document.cookie = 'userHasAcceptAnalytics=no'; // Créé ou met à jour un cookie 'user'
+    document.cookie.remove();
+
+})
